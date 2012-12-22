601319f99ff7de29c80133dcd3a8182abaaf7d9f
Andreas Fuchs <asf@boinkor.net>
2012-12-22 01:39:08 -0800

    Add two minimal tests
    At least let's ensure this thing can be loaded & generate XML.

--- Gemfile
+++ Gemfile
@@ -8,7 +8,9 @@ gem 'haml', '~> 3.1.6'
 group :development do
   gem "shoulda", ">= 0"
   gem "rdoc", "~> 3.12"
-  gem "bundler", "~> 1.1.0"
+  gem "bundler", "~> 1.2.0"
   gem "jeweler", "~> 1.8.4"
   gem "rcov", ">= 0"
+  gem "minitest"
+  gem "nokogiri"
 end
---
+++ test/test_gmail-britta.rb
@@ -0,0 +1,45 @@
+require 'bundler/setup'
+require 'minitest/unit'
+require 'nokogiri'
+require 'minitest/autorun'
+
+require 'gmail-britta'
+
+describe GmailBritta do
+  def simple_filterset
+    fs = GmailBritta.filterset() do
+      filter {
+        has 'to:asf@boinkor.net'
+        label 'ohai'
+      }
+    end
+  end
+
+  def dom(filterset)
+    text = simple_filterset.generate
+    #puts text
+    Nokogiri::XML.parse(text)
+  end
+
+  def ns
+    {
+      'a' => 'http://www.w3.org/2005/Atom',
+      'apps' => 'http://schemas.google.com/apps/2006'
+    }
+  end
+
+  it "runs" do
+    filters = simple_filterset.generate
+    assert(filters, "Should generate something")
+    assert(filters.is_a?(String), "Generated filters should be a string")
+  end
+
+  it "generates xml" do
+    filters = dom(simple_filterset)
+
+    assert_equal(1, filters.xpath('/a:feed/a:entry',ns).length, "Should have exactly one filter entry")
+    assert_equal(2, filters.xpath('/a:feed/a:entry/apps:property',ns).length, "Should have two properties")
+    assert_equal(1, filters.xpath('/a:feed/a:entry/apps:property[@name="label"]',ns).length, "Should have exactly one 'label' property")
+    assert_equal(1, filters.xpath('/a:feed/a:entry/apps:property[@name="hasTheWord"]',ns).length, "Should have exactly one 'has' property")
+  end
+end
